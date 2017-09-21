aadda403b988e0a68f350cded5e08b62dbf4a3a2
tom.louis.murphy <tom.louis.murphy@gmail.com>
2017-09-20 23:03:31 -0500

    commit 7 - factories

--- classexercises.js
+++ classexercises.js
@@ -86,3 +86,48 @@ class Person {
 }
 
 const lady = new Person('Jill');
+
+//Factory Problems
+
+class Shoe {
+	constructor(brand, serialNumber){
+		this.brand = brand;
+		this.serialNumber = serialNumber;
+	}
+}
+
+class Factory {
+	constructor(brand){
+		this.brand = brand;
+		this.shoes = [];
+	}
+	produceShoe(){
+		const newShoe = new Shoe(this.brand, this.shoes.length);
+		this.shoes.push(newShoe);
+		return newShoe;
+	}
+	findCar(index){
+		return this.shoe[index];
+	}
+}
+
+const kick = new Factory('Nike');
+
+class Store {
+	constructor(mallName, serialNumber){
+		this.mallName = mallName;
+		this.serialNumber = serialNumber;
+	}
+}
+
+class Mall {
+	constructor(mallName){
+		this.mallName = mallName;
+		this.stores = [];
+	}
+	generateStore(){
+		const newStore = new Store(this.mallName, this.stores.length);
+		this.stores.push(newStore);
+		return newStore;
+	}
+}
